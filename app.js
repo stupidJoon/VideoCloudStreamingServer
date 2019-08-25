71961eaa519e9a9295e0666c9d7a2bc7f3f54caa
stupidJoon <junsang.yu3@gmail.com>
2019-08-26 02:24:40 +0900

    a

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json())
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
